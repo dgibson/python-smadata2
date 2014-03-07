c490a118c14a24cfaad3ab8c88df6af473c3d325
David Gibson <david@gibson.dropbear.id.au>
2014-03-07 23:07:47 +1100

    Remove copy and paste error
    Not sure how I failed to catch that originally.  Oops.
    
    Signed-off-by: David Gibson <david@gibson.dropbear.id.au>

--- smadata2/db.py
+++ smadata2/db.py
@@ -17,7 +17,6 @@
 # with this program; if not, write to the Free Software Foundation, Inc.,
 # 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 
-protocol - Python code to communicate with SMA inverters
 from __future__ import print_function
 
 import sys
