2639bb50270f65dbaec4f27aa6ed2d442de52a7f
Jan Sandbrink <fischikowski@web.de>
2016-11-05 20:26:21 +0100

    Add very basic MySQL connector
    - no own migrations
    - sharing common base class with SQLite

--- smadata2/db/sqlite.py
+++ smadata2/db/sqlite.py
@@ -24,7 +24,7 @@ import shutil
 import re
 import sqlite3
 
-from base import *
+from sql import *
 
 
 all = ['SQLiteDatabase']
@@ -45,7 +45,7 @@ def sqlite_schema(conn):
     return squash_schema(sqls)
 
 
-class SQLiteDatabase(BaseDatabase):
+class SQLiteDatabase(SQLDatabase):
     DDL = [
 """CREATE TABLE generation (inverter_serial INTEGER,
                             timestamp INTEGER,
@@ -59,146 +59,11 @@ class SQLiteDatabase(BaseDatabase):
         super(SQLiteDatabase, self).__init__()
 
         self.conn = sqlite3.connect(filename)
+        self.placeholder = "?"
 
         schema = sqlite_schema(self.conn)
         if schema != squash_schema(self.DDL):
             raise WrongSchema("Incorrect database schema")
-
-    def commit(self):
-        self.conn.commit()
-
-    def add_historic(self, serial, timestamp, total_yield):
-        c = self.conn.cursor()
-        c.execute("INSERT INTO generation"
-                  + " (inverter_serial, timestamp, total_yield)"
-                  + " VALUES (?, ?, ?);",
-                  (serial, timestamp, total_yield))
-
-    def get_one_historic(self, serial, timestamp):
-        c = self.conn.cursor()
-        c.execute("SELECT total_yield FROM generation"
-                  " WHERE inverter_serial = ?"
-                  " AND timestamp = ?", (serial, timestamp))
-        r = c.fetchone()
-        if r is not None:
-            return r[0]
-
-    def get_last_historic(self, serial):
-        c = self.conn.cursor()
-        c.execute("SELECT max(timestamp) FROM generation"
-                  " WHERE inverter_serial = ?", (serial,))
-        r = c.fetchone()
-        return r[0]
-
-    def get_aggregate_one_historic(self, ts, ids):
-        c = self.conn.cursor()
-        c.execute("SELECT sum(total_yield) FROM generation"
-                  " WHERE inverter_serial IN(" + ",".join("?" * len(ids)) + ")"
-                  " AND timestamp = ?"
-                  " GROUP BY timestamp", tuple(ids) + (ts,))
-        r = c.fetchall()
-        if not r:
-            return None
-        assert(len(r) == 1)
-        return r[0][0]
-
-    def get_aggregate_historic(self, from_ts, to_ts, ids):
-        c = self.conn.cursor()
-        c.execute("SELECT timestamp, sum(total_yield) FROM generation"
-                  " WHERE inverter_serial IN (" + ",".join("?" * len(ids)) + ")"
-                  + " AND timestamp >= ? AND timestamp < ?"
-                  + " GROUP BY timestamp ORDER BY timestamp ASC",
-                  tuple(ids) + (from_ts, to_ts))
-        return c.fetchall()
-
-    # return midnights for each day in the database
-    # @param serial the inverter seial number to retrieve midnights for
-    # @return all midnights in database as datetime objects
-    def midnights(self, inverters):
-        c = self.conn.cursor()
-        serials = ','.join(x.serial for x in inverters)
-        c.execute("SELECT distinct(timestamp) "
-                  "FROM generation "
-                  "WHERE inverter_serial  in ( ? ) "
-                  "AND timestamp % 86400 = 0 "
-                  "ORDER BY timestamp ASC", (serials,))
-        r = c.fetchall()
-        r = map(lambda x: datetime.datetime.utcfromtimestamp(x[0]), r)
-        return r
-
-    def get_datapoint_totals_for_day(self, inverters, start_datetime):
-        c = self.conn.cursor()
-        before_datetime = start_datetime + datetime.timedelta(days=1)
-        start_unixtime = time.mktime(start_datetime.timetuple())
-        before_unixtime = time.mktime(before_datetime.timetuple())
-        serials = ','.join(x.serial for x in inverters)
-        c.execute("SELECT timestamp,sum(total_yield),count(inverter_serial) "
-                  "FROM generation "
-                  "WHERE inverter_serial in  ( ? ) "
-                  "AND timestamp >= ? and timestamp < ? "
-                  "group by timestamp "
-                  "ORDER BY timestamp ASC", (serials, start_unixtime,
-                                             before_unixtime))
-        r = c.fetchall()
-        return r
-
-    # fixed
-    def get_entries(self, inverters, timestamp):
-        c = self.conn.cursor()
-        serials = ','.join(x.serial for x in inverters)
-        c.execute("SELECT timestamp,total_yield,inverter_serial "
-                  "FROM generation "
-                  "WHERE inverter_serial in ( ? ) "
-                  "AND timestamp = ? "
-                  "ORDER BY timestamp DESC LIMIT ?", (serials, timestamp, 1))
-        r = c.fetchall()
-        if len(r) == 0:
-            return None
-        return r
-
-    def get_productions_younger_than(self, inverters, timestamp):
-        serials = ','.join(x.serial for x in inverters)
-        c = self.conn.cursor()
-        c.execute("SELECT timestamp,total_yield,count(inverter_serial) "
-                  "FROM generation "
-                  "WHERE inverter_serial in ( ? ) AND "
-                  " timestamp > ? "
-                  "group by timestamp "
-                  "ORDER BY timestamp ASC", (serials, str(timestamp)))
-        r = c.fetchall()
-        return r
-
-    def pvoutput_get_last_datetime_uploaded(self, sid):
-        c = self.conn.cursor()
-        c.execute("SELECT last_datetime_uploaded "
-                  "FROM pvoutput "
-                  "WHERE sid = ?", (sid,))
-        r = c.fetchone()
-        if r is None:
-            return None
-        return r[0]
-
-    def pvoutput_maybe_init_system(self, sid):
-        print(sid)
-        c = self.conn.cursor()
-        c.execute("SELECT * FROM pvoutput"
-                  " WHERE sid = ?",
-                  (sid,))
-        r = c.fetchone()
-        if r is None:
-            c = self.conn.cursor()
-            c.execute("INSERT INTO pvoutput(sid) VALUES ( ? )",
-                      (sid,))
-            self.commit()
-
-    def pvoutput_set_last_datetime_uploaded(self, sid, value):
-        c = self.conn.cursor()
-        self.pvoutput_maybe_init_system(sid)
-        c.execute("update pvoutput "
-                  "SET last_datetime_uploaded = ?"
-                  "WHERE sid = ?", (value, sid))
-        self.commit()
-
 
 SCHEMA_CURRENT = squash_schema(SQLiteDatabase.DDL)
 
